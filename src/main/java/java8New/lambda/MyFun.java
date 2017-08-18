da1b4f7d212c32eae2f5bd98b8c814606ab7a229
elonyong <elonyong@163.com>
2017-08-18 19:11:05 +0800

    lambda练习

---
+++ src/main/java/java8New/lambda/MyFun.java
@@ -0,0 +1,13 @@
+package java8New.lambda;
+
+/**
+ * @Name java8New.lambda.MyFun
+ * @Description
+ * @Author Elwyn
+ * @Version 2017/8/16
+ * @Copyright 上海云辰信息科技有限公司
+ **/
+@FunctionalInterface
+public interface MyFun {
+	Integer getValue(Integer integer);
+}
